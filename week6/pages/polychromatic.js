0c8119bc0b715fd158b1945627c91cd843f5bb70
Carlos from subway <tlee285@my.bcit.ca>
2023-02-14 10:58:58 -0800

    initial

---
+++ week6/pages/polychromatic.js
@@ -0,0 +1,91 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Image from "next/image"
+export default function Polychromatic () {
+const [image, setImage] = useState([])
+const [images, setImages] = useState([]);
+const [time, setTime] = useState("loading");
+const [date, setDate] = useState("");
+const [coords, setCoords] = useState({});
+const apiKey = "Rl653lAZk7HZOIe9EtUJqurVaIIl92bBbWab90su";
+const url = `https://epic.gsfc.nasa.gov/api/natural?api_key=${apiKey}`
+
+const getPolychromaticData = async () => {
+    const res = await axios.get(url);
+    const data = await res.data;
+    console.log(data)
+
+    const caption = data[0].caption
+    const date = data[0].date.split(" ")[0]
+
+    const date_formatted = date.replaceAll("-","/")
+    console.log(date_formatted)
+
+    let times =[];
+    let images = [];
+
+    for(let i = 0; i < data.length; i++){
+        let time = data[i].date.split("")[1];
+        let coords = data[i].centroid_coordinates;
+        let imagePath = data[i].image;
+        let image = `https://epic.gsfc.nasa.gov/archive/natural/${date_formatted}/png/${imagePath}.png`
+
+        times.push(time);
+        images.push({
+            image: image,
+            time: time,
+            coords: coords
+        })
+
+    }
+
+    setDate(date);
+    setImages(images)
+    setImage(images[0].image)
+    setTime(time[0])
+    setCoords([images[0].coords.lat, images[0].coords.lon])
+}
+    useEffect(() => {
+        getPolychromaticData();
+    },[])
+    return (
+        <div>
+            <Image src={image} alt={image} width={200} height={120}/>
+            <div>{time}</div>
+            <div>{coords[0]}, {coords[1]}</div>
+
+            <table> 
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Latitude</th>
+                        <th>Longitude</th>
+                        <th>Image</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        {
+                            images.map((e, i) => {
+                                return(
+                                    <tr key={i}>
+                                        <td>{e.time}</td>
+                                        <td>{e.coords.lat}</td>
+                                        <td>{e.coords.lon}</td>
+                                        <td><Image src={e.image} alt={i} width={200} height={130}/></td>
+                                        <td>
+                                        <button onClick={() => {
+                                            setImage(e.image);
+                                            setTime(e.time);
+                                            setCoords(e.coords.lat, e.coords.lon);
+                                            document.body.scrollIntoView();
+                                        }}>View here</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+            </table>
+        </div>
+    )
+}
